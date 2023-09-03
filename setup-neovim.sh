d54ca8c55fc260bab99277046f62f50995272a15
Daniel Hookins <daniel@hookins.net>
2023-09-03 22:46:37 +0930

    Update Neovim setup

--- setup-neovim.sh
+++ setup-neovim.sh
@@ -1,11 +1,18 @@
+echo "Installing Pre-requisites"
+sudo apt-get update &&\
+    sudo apt-get install -y ca-certificates curl gnup &&\
+    sudo mkdir -p /etc/apt/keyrings
+
 echo "Installing Neovim with Python extensions"
 sudo add-apt-repository ppa:neovim-ppa/stable &&\
-sudo apt-get update &&\
-sudo apt-get install -y neovim python3-neovim
+    sudo apt-get install -y neovim python3-neovim ripgrep fd-find
 
 echo "Installing NodeJS"
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x focal main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && \
+    sudo apt-get install nodejs -y
 
 echo "Installing Github Copilot and Nodejs"
 git clone https://github.com/github/copilot.vim.git \
@@ -15,5 +22,5 @@ echo "Github Copilot: Start Neovim and invoke :Copilot setup"
 echo "Installing Vim-Plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "Vim-Plug installed: Start Neovim and invoke :PlugInstall
+echo "Vim-Plug installed: Start Neovim and invoke :PlugInstall"
 
